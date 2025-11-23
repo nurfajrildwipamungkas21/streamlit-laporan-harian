d09ed9dcdf376e1f7cbe1a2ce5039d4739f63a29
nurfajrildwipamungkas21 <nurfajrildwipamungkas@gmail.com>
2025-11-23 20:09:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -504,7 +504,7 @@ if KONEKSI_GSHEET_BERHASIL:
             # 2. Kategori Aktivitas (Fitur Fairness dari Code 1)
             kategori_aktivitas = st.radio(
                 "Jenis Aktivitas:", 
-                ["🚗 Kunjungan Lapangan (Sales)", "💻 Digital Marketing / Konten / Ads", "📞 Telesales / Follow Up", "🏢 Admin / Lainnya"],
+                ["🚗 Sales", "💻 Digital Marketing / Konten / Ads", "📞 Telesales / Follow Up", "🏢 Lainnya"],
                 horizontal=True
             )
 
